d66957f205cbd1a330593d01a18fba0751a9e009
adityaaditiya <48347372+adityaaditiya@users.noreply.github.com>
2025-09-02 15:38:47 +0700

    Use sale receipt numbers in finance report

--- application/views/booking/my.php
+++ application/views/booking/my.php
@@ -13,6 +13,7 @@
             <th>Jam Selesai</th>
             <th>Status</th>
             <th>Keterangan</th>
+            <th>Bukti Pembayaran</th>
         </tr>
     </thead>
     <tbody>
@@ -24,10 +25,40 @@
             <td><?php echo htmlspecialchars($b->jam_selesai); ?></td>
             <td><?php echo htmlspecialchars(ucfirst($b->status_booking)); ?></td>
             <td><?php echo htmlspecialchars($b->keterangan); ?></td>
+            <td>
+                <?php if (!empty($b->bukti_pembayaran)): ?>
+                    <a href="#" class="preview-bukti" data-image="<?php echo base_url('uploads/payment_proofs/' . $b->bukti_pembayaran); ?>" title="Lihat bukti" aria-label="Lihat bukti"><i class="fas fa-eye"></i></a>
+                <?php else: ?>
+                    -
+                <?php endif; ?>
+            </td>
         </tr>
         <?php endforeach; ?>
     </tbody>
 </table>
 <?php endif; ?>
+<div class="modal fade" id="buktiModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-body text-center">
+                <img src="" alt="Bukti Pembayaran" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('.preview-bukti');
+    links.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            var imgSrc = this.getAttribute('data-image');
+            var modalImg = document.querySelector('#buktiModal img');
+            modalImg.src = imgSrc;
+            $('#buktiModal').modal('show');
+        });
+    });
+});
+</script>
 <?php $this->load->view('templates/footer'); ?>
 
