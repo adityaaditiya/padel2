ac98e694072f8fdd64fdafb2a74568a014a57793
adityaaditiya <48347372+adityaaditiya@users.noreply.github.com>
2025-09-06 17:18:04 +0700

    Add point rule model

---
+++ application/models/Reward_product_model.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Reward_product_model extends CI_Model
+{
+    protected $table = 'reward_products';
+    protected $log_table = 'reward_redemptions';
+
+    public function get_all()
+    {
+        return $this->db->get($this->table)->result();
+    }
+
+    public function get_by_id($id)
+    {
+        return $this->db->get_where($this->table, ['id' => $id])->row();
+    }
+
+    public function insert($data)
+    {
+        $this->db->insert($this->table, $data);
+        return $this->db->insert_id();
+    }
+
+    public function delete($id)
+    {
+        $this->db->where('id', $id)->delete($this->table);
+    }
+
+    public function reduce_stock($id, $qty = 1)
+    {
+        $this->db->set('stok', 'stok - ' . (int)$qty, false)
+                 ->where('id', $id)
+                 ->where('stok >=', $qty)
+                 ->update($this->table);
+    }
+
+    public function log_redemption($user_id, $reward_id)
+    {
+        $this->db->insert($this->log_table, [
+            'user_id'   => $user_id,
+            'reward_id' => $reward_id
+        ]);
+    }
+}
+?>
